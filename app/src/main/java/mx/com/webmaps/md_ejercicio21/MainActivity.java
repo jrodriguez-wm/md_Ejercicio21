c0f35e0af737570fa6c939ca646c32b2c9c6c392
Jorge Rodriguez <jorge.rodriguez@webmaps.com.mx>
2018-04-25 18:41:48 -0500

    Se crea un checkbox con estilos y su listener

--- app/src/main/java/mx/com/webmaps/md_ejercicio21/MainActivity.java
+++ app/src/main/java/mx/com/webmaps/md_ejercicio21/MainActivity.java
@@ -2,12 +2,32 @@ package mx.com.webmaps.md_ejercicio21;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    AppCompatCheckBox appCompatCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        appCompatCheckBox = (AppCompatCheckBox) findViewById(R.id.checkBox_id);
+
+        appCompatCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked){
+                    Toast.makeText(getApplicationContext(),"Check is Enable ",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Check is Disable ",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
